541651f807b84d4e1a8d3ad55c1c3bd7ef06f5cf
Alex Pirko <youzhny_foto@gmail.com>
2022-11-01 10:48:36 +0200

    feat: create task 4,5

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+const files = path.join(__dirname, 'files');
+const filesCopy = path.join(__dirname, 'copy');
+
+fs.rm(filesCopy, {recursive: true, force: true
+})
+.finally(function() {
+    fs.mkdir(filesCopy, {recursive: true
+    });
+    fs.readdir(files, {
+        withFileTypes: true
+    })
+    .then(function(elements) {
+        for (const elem of elements) {
+            if (elem.isFile()) {
+                let pathOrig = path.join(files, elem.name);
+                let pathCopy = path.join(filesCopy, elem.name);
+                fs.copyFile(pathOrig, pathCopy);}
+        };
+    });
+});
+      
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir } = require('fs/promises');
+const styles = path.join(__dirname, 'styles');
+const pathDir = path.join(__dirname, 'project-dist', 'bundle.css');
+
+const buildStream = fs.createWriteStream(pathDir, 'utf-8');
+async function buildBundle(input) {
+    try {
+        const files = await readdir(input, { withFileTypes: true });
+        files.forEach(function (file) {
+            if (file.isFile() && (path.extname(path.join(input, file.name)) == '.css' )) {
+            const inputStream = fs.createReadStream(path.join(input, file.name), 'utf-8');
+                inputStream.on('data', (chunk) => {
+                    buildStream.write(chunk);
+                });
+            }
+        });
+    } 
+    catch (error) {
+        console.log(error.message);
+    }
+}
+buildBundle(styles); 
+
+
